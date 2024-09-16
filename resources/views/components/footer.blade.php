1a6d15aa0d5b5adcc8262fe88a9b5411175e9df0
janis147 <jupitersaule@gmail.com>
2024-09-16 15:13:33 +0300

    init

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,13 +1,15 @@
 <footer class="main-footer padding-side-lg">
     <div class="footer-items">
         <div class="left">
+            <x-carbon-language class="language-icon"/>
             <span>English (INTL)</span>
         </div>
         <div class="right">
-
+            <a href="">Legal & privacy</a>
+            <a href="">Cookies</a>
         </div>
     </div>
     <div class="logo-box">
-
+        <a href="{{route('home')}}">GIANT</a>
     </div>
 </footer>
